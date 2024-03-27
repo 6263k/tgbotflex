229f36516fd9052a78b102f7b1b686582d8ac0e3
Blinov <>
2024-03-27 20:33:34 +0300

    Изменения

--- tgbotflex/TGHandlers/DefaultBotHandlers.swift
+++ tgbotflex/TGHandlers/DefaultBotHandlers.swift
@@ -13,66 +13,101 @@ final class DefaultBotHandlers {
     static func addHandlers(app: Vapor.Application, connection: TGConnectionPrtcl) async {
         await defaultBaseHandler(app: app, connection: connection)
         await messageHandler(app: app, connection: connection)
-        await commandPingHandler(app: app, connection: connection)
         await commandShowButtonsHandler(app: app, connection: connection)
         await buttonsActionHandler(app: app, connection: connection)
     }
     
     private static func defaultBaseHandler(app: Vapor.Application, connection: TGConnectionPrtcl) async {
-        await connection.dispatcher.add(TGBaseHandler({ update, bot in
-            guard let message = update.message else { return }
-            let params: TGSendMessageParams = .init(chatId: .chat(message.chat.id), text: "TGBaseHandler")
-            try await bot.sendMessage(params: params)
-        }))
+		let tgBaseHandler = TGBaseHandler() { update, bot in
+			guard let message = update.message else { return }
+//			let params = TGSendMessageParams(chatId: .chat(message.chat.id), text: "TGBaseHandler")
+//			try await bot.sendMessage(params: params)
+		}
+        await connection.dispatcher.add(tgBaseHandler)
     }
 
     private static func messageHandler(app: Vapor.Application, connection: TGConnectionPrtcl) async {
-        await connection.dispatcher.add(TGMessageHandler(filters: (.all && !.command.names(["/ping", "/show_buttons"]))) { update, bot in
-            let params: TGSendMessageParams = .init(chatId: .chat(update.message!.chat.id), text: "Success")
-            try await bot.sendMessage(params: params)
-        })
-    }
-
-    private static func commandPingHandler(app: Vapor.Application, connection: TGConnectionPrtcl) async {
-        await connection.dispatcher.add(TGCommandHandler(commands: ["/ping"]) { update, bot in
-            try await update.message?.reply(text: "pong", bot: bot)
-        })
+		let messageHandler = TGMessageHandler(filters: (.all && !.command.names(["/ping", "/show_buttons"]))) { update, bot in
+//			let params = TGSendMessageParams(chatId: .chat(update.message!.chat.id), text: "Success")
+//			try await bot.sendMessage(params: params)
+		}
+        await connection.dispatcher.add(messageHandler)
     }
 
     private static func commandShowButtonsHandler(app: Vapor.Application, connection: TGConnectionPrtcl) async {
-        await connection.dispatcher.add(TGCommandHandler(commands: ["/show_buttons"]) { update, bot in
-            guard let userId = update.message?.from?.id else { fatalError("user id not found") }
-            let buttons: [[TGInlineKeyboardButton]] = [
-                [.init(text: "Button 1", callbackData: "press 1"), .init(text: "Button 2", callbackData: "press 2")]
-            ]
-            let keyboard: TGInlineKeyboardMarkup = .init(inlineKeyboard: buttons)
-            let params: TGSendMessageParams = .init(chatId: .chat(userId),
-                                                    text: "Keyboard active",
-                                                    replyMarkup: .inlineKeyboardMarkup(keyboard))
-            try await bot.sendMessage(params: params)
-        })
+		let commandHandler = TGCommandHandler(commands: ["/start"]) { update, bot in
+			guard let userId = update.message?.from?.id else {
+//				fatalError("user id not found")
+				return
+			}
+			try await bot.sendMessage(params: TGSendMessageParams(chatId: .chat(userId), text: "Приветствуем 😁"))
+			let buttons: [[TGInlineKeyboardButton]] = [
+				[
+					TGInlineKeyboardButton(text: "💎 Telegram", callbackData: "TGDefault"),
+					TGInlineKeyboardButton(text: "💎 Telegram Premium", callbackData: "press 2")
+				],
+				[
+					TGInlineKeyboardButton(text: "💎 Instagram", callbackData: "press 1"),
+					TGInlineKeyboardButton(text: "💎 TikTok", callbackData: "press 1"),
+				],
+				[
+					TGInlineKeyboardButton(text: "💎 VK", callbackData: "press 1"),
+					TGInlineKeyboardButton(text: "💎 YouTube", callbackData: "press 1"),
+				],
+				[TGInlineKeyboardButton(text: "NOLOYALTY CLUB", callbackData: "press 1")]
+			]
+			let keyboard = TGInlineKeyboardMarkup(inlineKeyboard: buttons)
+			let params = TGSendMessageParams(
+				chatId: .chat(userId),
+				text: Localizable.Titles.greetings,
+				replyMarkup: .inlineKeyboardMarkup(keyboard)
+			)
+			try await bot.sendMessage(params: params)
+		}
+        await connection.dispatcher.add(commandHandler)
     }
 
     private static func buttonsActionHandler(app: Vapor.Application, connection: TGConnectionPrtcl) async {
-        await connection.dispatcher.add(TGCallbackQueryHandler(pattern: "press 1") { update, bot in
-            TGBot.log.info("press 1")
-            let params: TGAnswerCallbackQueryParams = .init(callbackQueryId: update.callbackQuery?.id ?? "0",
-                                                            text: update.callbackQuery?.data  ?? "data not exist",
-                                                            showAlert: nil,
-                                                            url: nil,
-                                                            cacheTime: nil)
-            try await bot.answerCallbackQuery(params: params)
-        })
-        
-        await connection.dispatcher.add(TGCallbackQueryHandler(pattern: "press 2") { update, bot in
-            TGBot.log.info("press 2")
-            let params: TGAnswerCallbackQueryParams = .init(callbackQueryId: update.callbackQuery?.id ?? "0",
-                                                            text: update.callbackQuery?.data  ?? "data not exist",
-                                                            showAlert: nil,
-                                                            url: nil,
-                                                            cacheTime: nil)
-            try await bot.answerCallbackQuery(params: params)
-        })
+		let tgLootKeeper = TGCallbackQueryHandler(pattern: "TGDefault") { update, bot in
+			guard let userId = update.message?.from?.id else {
+				return
+			}
+			let params = TGAnswerCallbackQueryParams(
+				callbackQueryId: update.callbackQuery?.id ?? "0",
+				text: update.callbackQuery?.data ?? "data not exist",
+				showAlert: false,
+				url: nil,
+				cacheTime: nil
+			)
+			try await bot.answerCallbackQuery(params: params)
+		}
+		await connection.dispatcher.add(tgLootKeeper)
+
+		let callbackHandler = TGCallbackQueryHandler(pattern: "press 1") { update, bot in
+			TGBot.log.info("press 1")
+			let params = TGAnswerCallbackQueryParams(
+				callbackQueryId: update.callbackQuery?.id ?? "0",
+				text: update.callbackQuery?.data  ?? "data not exist",
+				showAlert: nil,
+				url: nil,
+				cacheTime: nil
+			)
+			try await bot.answerCallbackQuery(params: params)
+		}
+        await connection.dispatcher.add(callbackHandler)
+
+		let queryHandler = TGCallbackQueryHandler(pattern: "press 2") { update, bot in
+			TGBot.log.info("press 2")
+			let params = TGAnswerCallbackQueryParams(
+				callbackQueryId: update.callbackQuery?.id ?? "0",
+				text: update.callbackQuery?.data  ?? "data not exist",
+				showAlert: nil,
+				url: nil,
+				cacheTime: nil
+			)
+			try await bot.answerCallbackQuery(params: params)
+		}
+        await connection.dispatcher.add(queryHandler)
     }
 }
 
--- tgbotflex/configure.swift
+++ tgbotflex/configure.swift
@@ -10,14 +10,10 @@ import Vapor
 import TelegramVaporBot
 
 public func configure(_ app: Application) async throws {
-    let tgApi: String = "XXXXXXXXXX:YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
-    /// set level of debug if you needed
-//    TGBot.log.logLevel = .error
+    let tgApi: String = "6780939740:AAHzOupHPh5PFDl9I3BjQBzqyucxs-yjtUw"
     TGBot.log.logLevel = app.logger.logLevel
     let bot: TGBot = .init(app: app, botId: tgApi)
     await TGBOT.setConnection(try await TGLongPollingConnection(bot: bot))
-    /// OR SET WEBHOOK CONNECTION
-    /// await TGBOT.setConnection(try await TGWebHookConnection(bot: bot, webHookURL: "https://your_domain/telegramWebHook"))
     
     await DefaultBotHandlers.addHandlers(app: app, connection: TGBOT.connection)
     try await TGBOT.connection.start()
